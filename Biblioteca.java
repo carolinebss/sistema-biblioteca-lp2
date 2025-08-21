3310335e80c6a461b5bbf4b0acf6b2fdf4b92846
Caroline Barbosa <carolinebsbarbosa26@gmail.com>
2025-08-21 15:46:57 -0300

    Add files via upload

---
+++ Biblioteca.java
@@ -0,0 +1,7 @@
+public class Biblioteca {
+    public static void main(String[] args) {
+        Livro livro1 = new Livro(titulo: "Como destruir a sua vida", autor: "Caroline Barbosa", ano: 2025);
+        System.out.println(livro1);    
+    }
+
+}
---
+++ Livro.java
@@ -0,0 +1,61 @@
+public class Livro {
+    private String titulo;
+    private String autor;
+    private int ano; 
+
+    public Livro(String titulo, String autor, int ano) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.ano = ano;
+    }
+
+    
+    public String getTitulo() {
+        return titulo;
+    }
+
+
+    public void setTitulo(String titulo) {
+        if (titulo == ""){
+            System.out.println("Erro: titulo inválido");
+        } else {
+            this.titulo = titulo;
+        }
+    }
+
+
+    public String getAutor() {
+        return autor;
+    }
+
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+
+    public int getAno() {
+        return ano;
+    }
+
+
+    public void setAno(int ano) {
+        int ano_atual = 2025;
+        if (ano > ano_atual) {
+            System.out.println("Erro: ano inválido.");
+        } else {
+            this.ano = ano;
+        }
+    }
+
+
+    @Override
+    public String toString() {
+        return "Livro{" + 
+        "titulo='"
+        super.toString();
+    }
+    
+}
+
+
