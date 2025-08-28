f6f159d66dbb393ac910cdbb918f44c531db1ea0
Caroline Barbosa <carolinebsbarbosa26@gmail.com>
2025-08-28 16:00:49 -0300

    Update Biblioteca.java

--- Biblioteca.java
+++ Biblioteca.java
@@ -1,96 +1,65 @@
-import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.List;
+public class Livro {
+    private String titulo;
+    private String autor;
+    private int ano; 
+    private StatusLivro status;
 
-public class Biblioteca {
-        private List<Livro> acervo;
-        private List<Usuario> ListadeUsuarios;
-        private List<Emprestimo> registrosDeEmprestimos;
+    public Livro(String titulo, String autor, int ano) {
+        setTitulo(titulo)
+        setAutor(autor)
+        setAno(ano)
+    }
 
-        public Biblioteca() {
-            this.acervo = new ArrayList<>();
-            this.ListadeUsuarios = new ArrayList<>();
-            this.registrosDeEmprestimos = new ArrayList<>();
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        if (titulo == ""){
+            System.out.println("Erro: titulo inválido");
+        } else {
+            this.titulo = titulo;
         }
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        if (autor == "") {
+            System.out.println("Erro: autor inválido");
+        } else {
+            this.autor = autor;
+        }
+    }
+
+    public int getAno() {
+        return ano;
+    }
+
+    public void setAno(int ano) {
+        int ano_atual = 2025;
+        if (ano > ano_atual) {
+            System.out.println("Erro: ano inválido.");
+        } else {
+            this.ano = ano;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Livro '" + titulo "', de"  + autor + " (" + ano + ") - Status: " + status;
         
-        public void realizarEmprestimo (String idUsuario, String titulo) {
-            Usuario usuarioDoEmprestimo = pesquisarPorUsuarioPorId(idUsuario);
-            if(usuarioDoEmprestimo == null) {
-                System.out.println("Erro: esse usuário não está cadastrado.");
-                return;
-            }
-            Livro livroDoEmprestimo = pesquisarLivroPorTitulo(titulo);
-            if(livroDoEmprestimo == null) {
-                System.out.println("Erro: esse livro não existe.");
-                return;
-            }
-            if(livroDoEmprestimo.getStatus() == StatusLivro.EMPRESTADO) {
-                System.out.println("Erro: esse livro já está emprestado.");
-                return;
-            }
-            livroDoEmprestimo.setStatus(StatusLivro.EMPRESTADO);
-            Emprestimo emprestimo = new Emprestimo(livroDoEmprestimo, usuarioDoEmprestimo, LocalDate.now());
-            registrosDeEmprestimos.add(emprestimo);
-            System.out.println("Emprestimo cadastrado com sucesso.");
-        }
-
-        public void pesquisarPorUsuario(String id) {
-            for(Usuario usuario: this.ListadeUsuarios){
-                if(usuario.getId().equalsIgnoreCase(id)){
-                    return id;
-                }
-            }
-            return null;
-        }
-
-        public void pesquisarPorTitulo(String titulo) {
-            for(Livro livro : this.acervo){
-                if(livro.getTitulo().toLowerCase().equalsIgnoreCase(titulo)){
-                    return livro;
-                }
-            }
-            return null;
-        }
-        public List<Livro> pesquisarLivroPorTermo(String termo) {
-            List<Livro> listaDeLivros = new ArrayList<>();
-            for(Livro livro : acervo) {
-                if(livro.getTitulo().toLowerCase().contains(termo.toLowerCase())) {
-                    listaDeLivros.add(livro);
-                }
-            }
-
-        }
-        public void listarAcervo() {
-            for(Livro livro : acervo) {
-                System.out.println(livro);
-            }
-
-        }
-        public void cadastratrarLivro(Livro livro) {
-            this.acervo.add(livro);
-            System.out.println("O livro" + livro.getTitulo() + "foi cadastrado");
-
-        }
-        public void cadastratrarUsuario(Usuario usuario) {
-            this.ListadeUsuarios.add(usuario);
-            System.out.println("O Usuario" + usuario.getNome() + "foi cadastrado");
-
-        }
-        public void main(String[] args) {
-            Livro livro1 = new Livro("Como destruir a sua vida", "Caroline Barbosa", 2025);
-            Usuario meuUsuario = new Usuario("Caroll", "123");
-            Biblioteca minhaBiblioteca = new Biblioteca();
-            minhaBiblioteca.cadastratrarLivro(livro1);
-            minhaBiblioteca.cadastratrarUsuario(meuUsuario);
-            List<Livro> resultado = minhaBiblioteca.pesquisarLivroPorTermo("java");
-            for (var livro : resultado) {
-                System.out.println("Livros encontrados");
-                System.out.println(livro);
-            }
-
-           
     }
-
+    public StatusLivro getStatus() {
+        return status;
 
     }
+    public void setStatus(StatusLivro status) {
+        this.status = status;
+    }
+    
+}
+
 
