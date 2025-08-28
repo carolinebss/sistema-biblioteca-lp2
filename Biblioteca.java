ba89ac4d6f2fa106631781b15c023b997adf6b58
Caroline Barbosa <carolinebsbarbosa26@gmail.com>
2025-08-28 15:58:46 -0300

    Update Biblioteca.java

--- Biblioteca.java
+++ Biblioteca.java
@@ -1,15 +1,49 @@
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Biblioteca {
         private List<Livro> acervo;
         private List<Usuario> ListadeUsuarios;
+        private List<Emprestimo> registrosDeEmprestimos;
 
         public Biblioteca() {
             this.acervo = new ArrayList<>();
             this.ListadeUsuarios = new ArrayList<>();
+            this.registrosDeEmprestimos = new ArrayList<>();
         }
-        public void pesquisaPorTitulo(String titulo) {
+        
+        public void realizarEmprestimo (String idUsuario, String titulo) {
+            Usuario usuarioDoEmprestimo = pesquisarPorUsuarioPorId(idUsuario);
+            if(usuarioDoEmprestimo == null) {
+                System.out.println("Erro: esse usuário não está cadastrado.");
+                return;
+            }
+            Livro livroDoEmprestimo = pesquisarLivroPorTitulo(titulo);
+            if(livroDoEmprestimo == null) {
+                System.out.println("Erro: esse livro não existe.");
+                return;
+            }
+            if(livroDoEmprestimo.getStatus() == StatusLivro.EMPRESTADO) {
+                System.out.println("Erro: esse livro já está emprestado.");
+                return;
+            }
+            livroDoEmprestimo.setStatus(StatusLivro.EMPRESTADO);
+            Emprestimo emprestimo = new Emprestimo(livroDoEmprestimo, usuarioDoEmprestimo, LocalDate.now());
+            registrosDeEmprestimos.add(emprestimo);
+            System.out.println("Emprestimo cadastrado com sucesso.");
+        }
+
+        public void pesquisarPorUsuario(String id) {
+            for(Usuario usuario: this.ListadeUsuarios){
+                if(usuario.getId().equalsIgnoreCase(id)){
+                    return id;
+                }
+            }
+            return null;
+        }
+
+        public void pesquisarPorTitulo(String titulo) {
             for(Livro livro : this.acervo){
                 if(livro.getTitulo().toLowerCase().equalsIgnoreCase(titulo)){
                     return livro;
@@ -21,7 +55,7 @@ public class Biblioteca {
             List<Livro> listaDeLivros = new ArrayList<>();
             for(Livro livro : acervo) {
                 if(livro.getTitulo().toLowerCase().contains(termo.toLowerCase())) {
-                    listaDeLivros.add(livro)
+                    listaDeLivros.add(livro);
                 }
             }
 
@@ -32,7 +66,6 @@ public class Biblioteca {
             }
 
         }
-
         public void cadastratrarLivro(Livro livro) {
             this.acervo.add(livro);
             System.out.println("O livro" + livro.getTitulo() + "foi cadastrado");
