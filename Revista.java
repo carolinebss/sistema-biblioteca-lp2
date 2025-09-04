f99effeaaaeb90949ba1b7f8d7db5d49be374733
Caroline Barbosa <carolinebsbarbosa26@gmail.com>
2025-09-04 15:56:43 -0300

    Create Revista.java

---
+++ Revista.java
@@ -0,0 +1,19 @@
+public class Revista extends ItemDoAcervo{
+
+    private int edicao;
+
+    public Revista(String titulo, int ano, int edicao) {
+        super(titulo, ano);
+        setEdicao(edicao);        
+    }
+    public int getEdicao() {
+        return edicao;
+    }
+    public void setEdicao(int edicao) {
+        this.edicao = edicao;
+    }
+    @Override
+    public String toString() {
+         return "Revista '" + getTitulo() + " (" + getAno() + ") - Status: " + getStatus() + "Edição -" + edicao;
+    }
+}
