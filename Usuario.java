2278ee30cd8c693c361ae8996b4cff6155ef9850
Caroline Barbosa <carolinebsbarbosa26@gmail.com>
2025-09-04 15:55:20 -0300

    Update Usuario.java

--- Usuario.java
+++ Usuario.java
@@ -1,64 +1,18 @@
-public class Livro {
-    private String titulo;
-    private String autor;
-    private int ano; 
-    private StatusLivro status;
 
-    public Livro(String titulo, String autor, int ano) {
-        setTitulo(titulo);
-        setAutor(autor);
-        setAno(ano);
-        this.status = StatusLivro.DISPONIVEL;
+public class Usuario {
+
+    public Usuario(String string, String string2) {
+        //TODO Auto-generated constructor stub
     }
 
-    public String getTitulo() {
-        return titulo;
+    public String getNome() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getNome'");
     }
 
-    public void setTitulo(String titulo) {
-        if (titulo == ""){
-            System.out.println("Erro: titulo inválido");
-        } else {
-            this.titulo = titulo;
-        }
+    public String getId() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getId'");
     }
 
-    public String getAutor() {
-        return autor;
-    }
-
-    public void setAutor(String autor) {
-        if (autor == "") {
-            System.out.println("Erro: autor inválido");
-        } else {
-            this.autor = autor;
-        }
-    }
-
-    public int getAno() {
-        return ano;
-    }
-
-    public void setAno(int ano) {
-        int ano_atual = 2025;
-        if (ano > ano_atual) {
-            System.out.println("Erro: ano inválido.");
-        } else {
-            this.ano = ano;
-        }
-    }
-    
-    @Override
-    public String toString() {
-         return "Livro '" + titulo + "', de " + autor + " (" + ano + ") - Status: " + status;
-    }
-    
-    public StatusLivro getStatus() {
-        return status;
-
-    }
-    public void setStatus(StatusLivro status) {
-        this.status = status;
-    }
-    
 }
