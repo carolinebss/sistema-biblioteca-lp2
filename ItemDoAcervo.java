0484bfd18f1034aa0ce44347506b474111680955
Caroline Barbosa <carolinebsbarbosa26@gmail.com>
2026-02-15 23:17:01 -0300

    Update ItemDoAcervo.java
    EXERCICIO NOVO

--- ItemDoAcervo.java
+++ ItemDoAcervo.java
@@ -1,19 +1,16 @@
-public class ItemDoAcervo {
-    private String titulo;
-    private int ano; 
-    private StatusItem status;
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
 
-    public ItemDoAcervo(String titulo, int ano) {
-        setTitulo(titulo);
-        setAno(ano);
-        status = StatusLivro.DISPONIVEL;
-    }
-     
-    public String getAno() {
+public abstract class ItemDoAcervo implements Imprimivel {
+
+    public String titulo;
+    public int ano;
+    public StatusItem status;
+
+    public int getAno() {
         return ano;
     }
-
-     public void setAno(int ano) {
+    public void setAno(int ano) {
         int ano_atual = 2025;
         if (ano > ano_atual) {
             System.out.println("Erro: ano inválido.");
@@ -21,29 +18,33 @@ public class ItemDoAcervo {
             this.ano = ano;
         }
     }
-
     public String getTitulo() {
         return titulo;
     }
 
     public void setTitulo(String titulo) {
-        if (titulo == "") {
+        if (titulo == null || titulo.isEmpty()) {
             System.out.println("Erro: título inválido.");
         } else {
             this.titulo = titulo;
         }
     }
-    
-    public abstract int getPrazoEmprestimoDias();
-    public abstract double getValorMultaPorDiasAtraso();
-
-    public StatusLivro getStatus() {
+    public StatusItem getStatus() {
         return status;
     }
-
-    public void setStatus(StatusLivro status) {
+    public void setStatus(StatusItem status) {
         this.status = status;
+    }
+    public ItemDoAcervo (String titulo, int ano){
+        setTitulo(titulo);
+        setAno(ano);
+        setStatus(StatusItem.DISPONIVEL);
+    }
+    public int getPrazo(){
+        return 7;
+    }
+    public double getMulta_Por_dia(){
+        return 0.50;
     }
 
 }
-
