e07232206b133a8c1d76d39aed76592087d5122c
Caroline Barbosa <carolinebsbarbosa26@gmail.com>
2026-02-15 23:14:00 -0300

    Update Livro.java
    EXERCICIO NOVO

--- Livro.java
+++ Livro.java
@@ -1,35 +1,58 @@
-public class Livro extends ItemDoAcervo{
 
+public class Livro extends ItemDoAcervo implements Reservavel, Validavel {
+    private boolean reservado;
     private String autor;
+    private String isbn;
 
-    public Livro(String titulo, int ano, String autor) {
+
+    public Livro(String titulo, String autor, int ano, String isbn) {
         super(titulo, ano);
         setAutor(autor);
+        this.isbn = isbn;
     }
     public String getAutor() {
         return autor;
     }
     public void setAutor(String autor) {
-        if (autor == "") {
-            System.out.println("Erro: autor inválido");
+        if (autor == null|| autor.isEmpty()) {
+            System.out.println("Erro: título inválido.");
         } else {
             this.autor = autor;
         }
     }
-     @Override
-    public int getPrazoEmprestimoDias() {
-        return 10;
-    }
 
+    @Override
+    public int getPrazo() {
+        return 14;
+    }
     @Override
     public String toString() {
-         return "Livro '" + getTitulo() + "', de " + autor + " (" + getAno() + ") - Status: " + getStatus() ;
+        return "Livro '" + getTitulo() + "', de " + super.toString() + "Autor:" + autor + " (" + getAno() + ") - Status: " + getStatus();
     }
     @Override
-    public String getDadosParaBusca() {
-         return getTitulo() + " " + autor;
+    public double getMulta_Por_dia() {
+        return 0.75;
     }
+    @Override
+    public String formatarParaEtiqueta() {
+        return "LIVRO - Título: " + getTitulo() + " | Autor: " + autor;
+    }
+    @Override
+    public void reservar() {
+        reservado = true;
+    }
+
+    @Override
+    public void cancelarReserva() {
+        reservado = false;
+    }
+    public boolean isReservado() {
+        return reservado;
+    }
+    @Override
+    public boolean validar() {
+        return getTitulo() != null && !getTitulo().isEmpty()
+                && isbn != null && isbn.length() == 13;
+    }
+
 }
-
-
-
